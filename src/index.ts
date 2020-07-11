0154ae099120c50412b4b906b0186092ec4c12ce
8y8x <61947825+8y8x@users.noreply.github.com>
2020-07-11 15:27:27 -0700

    Add checks if storage doesn't exist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
+import { promises as fs } from 'fs';
+
 import getWeather from './getWeather';
 import saveWeather from './saveWeather';
 
 console.log('OK');
+
+async function checkStorage() {
+    return new Promise(async r => {
+        try {
+            await fs.readdir('./storage');
+        } catch(err) {
+            await fs.mkdir('./storage');
+        }
+
+        try {
+            await fs.access('./storage/temperatures.json');
+        } catch(err) {
+            await fs.writeFile('./storage/temperatures.json', '[]');
+        }
+    });
+}
 
 async function main() {
     return new Promise(async r => {
@@ -26,7 +44,7 @@ async function main() {
     });
 }
 
-main();
+checkStorage().then(main);
 setInterval(main, 1e3 * 60 * 60); // Call every hour
 
 require('./httpServer');
